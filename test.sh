c9ed9b2516e55a78251f626286e30e19b2ced0d0
elcolin <elise.col8@gmail.com>
2026-01-27 14:06:54 +0100

    tests: added unit tests for argument handling * Below and equal zero threads and ports. * No string given in scan field.

--- test.sh
+++ test.sh
@@ -29,16 +29,21 @@ run_test "--speedup test (invalid)"     1  --speedup test
 run_test " --ports 1 (valid)"           0 $default_ip --ports 1
 run_test "--speedup 1 (valid)"          0 $default_ip --speedup 1
 run_test "--speedup 251 (invalid)"      1 $default_ip --speedup 251
+run_test "--speedup -2 (invalid)"       1 $default_ip --speedup -2
+run_test "--speedup 0 (invalid)"       1 $default_ip --speedup 0
 run_test "--ports 65536 (invalid)"      1 $default_ip --ports 65536
+run_test "--ports -2 (invalid)"         1 $default_ip --ports -2
+run_test "--ports 0 (invalid)"         1 $default_ip --ports 0
 run_test "--scan SYN (valid)"           0 $default_ip --scan SYN
 run_test "--scan NULL (valid)"          0 $default_ip --scan NULL
 run_test "--scan ACK (valid)"           0 $default_ip --scan ACK
 run_test "--scan FIN (valid)"           0 $default_ip --scan FIN
 run_test "--scan XMAS (valid)"          0 $default_ip --scan XMAS
 run_test "--scan (invalid) aaaa"        1 $default_ip --scan aaa
+run_test "--scan (invalid) """          1 $default_ip --scan ""
 run_test "--ip (valid) 8.8.8.8"         0 --ip 8.8.8.8
 run_test "--ip (valid) google.com"      0 --ip google.com
 run_test "--ip (invalid) nope"          1 --ip nope
-run_test "(invalid) no IP"                    1 ""
+run_test "(invalid) no IP"              1 ""
 
 run_test "--ports 1 $default_ip --speedup 250 --scan SYN" 0 --ports 1 $default_ip --speedup 250 --scan SYN
